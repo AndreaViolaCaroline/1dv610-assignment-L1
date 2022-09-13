001c53403ba8afaeddc14d444be4e250483f85c0
Caroline Akesson <ca223pw@student.lnu.se>
2022-09-13 12:12:56 -0500

    Add the convert FROM kilometer per hour class

--- wind-speed-converter/src/KilometerPerHour.js
+++ wind-speed-converter/src/KilometerPerHour.js
@@ -14,4 +14,40 @@ export class KilometerPerHour {
 
     return ms
   }
+
+  /**
+   * Converts value from kilometer per hour to feet per second.
+   *
+   * @param {number} kmh - The kmh value.
+   * @return - The feet per second value.
+   */
+   convertToFeetPerSecond (kmh) {
+    const feetPerSecond = kmh * 0.911344
+
+    return feetPerSecond
+  }
+
+  /**
+   * Converts value from kilometer per hour to miles per hour.
+   *
+   * @param {number} kmh - The kmh value.
+   * @return - The miles per hour value.
+   */
+   convertToMilesPerHour (kmh) {
+    const milesPerHour = kmh / 1.609344
+
+    return milesPerHour
+  }
+
+  /**
+   * Converts value from kilometer per hour to knots.
+   *
+   * @param {number} kmh - The kmh value.
+   * @return - The knots value.
+   */
+   convertToKnots (kmh) {
+    const knots = kmh * 0.539957
+
+    return knots
+  }
 }
